6f8a1ce30380f5bcc7d42064524f6bc6061759cf
Ishaa-23 <68493773+Ishaa-23@users.noreply.github.com>
2021-07-22 20:02:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function booked()
+{
+document.getElementById('inp_n').value="";
+document.getElementById('inp_nm').value="";
+document.getElementById('inp_d').value="";
+
+}
+
+function submit()
+{
+    document.getElementById('inp_f').value="";
+}
